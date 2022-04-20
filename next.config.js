0fdff7be1439b0394c02d905d6f2f5ba34728ab1
Alvaro Hugo Garcia Cespedes <alvaro.garcia.c2022@gmail.com>
2022-04-20 11:36:05 -0400

    DC

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
@@ -10,4 +11,5 @@ const nextConfig = {
 }
   
 module.exports = nextConfig
-   
+
+
